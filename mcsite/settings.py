28d6ae758bd7e403c7a8f8bc16c78e50b7c797b1
nursaProger <nursakeneshbb06@gmail.com>
2025-07-01 16:36:31 +0600

    two

--- mcsite/settings.py
+++ mcsite/settings.py
@@ -44,7 +44,7 @@ TEMPLATES = [
         },
     },
 ]
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main', 'static')]
 # ... existing code ... 
